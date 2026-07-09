72982394c567581c4df45fa7f587865570f5f732
骇客速成计划 <xsilver566@gmail.com>
2026-07-10 00:56:40 +0800

    Update: 2026-07-10 - Add new event class

--- TBasic/main.cpp
+++ TBasic/main.cpp
@@ -1,10 +1,12 @@
 #include "XBasic/XLog.h"
+#include "XBasic/EventSystem/Event.h"
 
 
 int main(int argc, char** argv) {
 
     xbasic::xlog::InitLogging(argv[0]);
     xbasic::xlog::XFlags_log_dir = std::string("../log");
+    xbasic::Event<int> event;
     // xbasic::xlog::XFlags_log_to_stdout = true;
     // xbasic::xlog::XFlags_log_to_stderr = true;
     //xbasic::xlog::SetLogDestination(xbasic::xlog::XLOG_INFO, "../log/log");
