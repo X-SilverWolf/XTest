44f0ebdcfde81f07c3e5f53bef6a565e0e733557
骇客速成计划 <xsilver566@gmail.com>
2026-09-05 00:59:34 +0800

    Update: 2026-09-05 - Add the color class and its func

--- TBasic/main.cpp
+++ TBasic/main.cpp
@@ -1,16 +1,21 @@
 #include "XBasic/XLog.h"
+#include "XBasic/App/AppBase.h"
+#include "XBasic/App/GLWindow.h"
+#include "XBasic/App/MainLoop.h"
 #include "XBasic/EventSystem/Event.h"
 
 
 int main(int argc, char** argv) {
 
     xbasic::xlog::InitLogging(argv[0]);
-    xbasic::xlog::XFlags_log_dir = std::string("../log");
-    xbasic::Event<int> event;
-    // xbasic::xlog::XFlags_log_to_stdout = true;
-    // xbasic::xlog::XFlags_log_to_stderr = true;
-    //xbasic::xlog::SetLogDestination(xbasic::xlog::XLOG_INFO, "../log/log");
-    // xbasic::LogSystem l;
+    xbasic::xlog::XFlags_log_dir = std::string("../../../Log");
+    xbasic::GLWindowSettings settings;
+    settings.setSize(1024, 768);
+    settings.windowMode = xbasic::eWindowMode_Window;
+    std::shared_ptr<xbasic::MainLoop> mainLoop = std::shared_ptr<xbasic::MainLoop>(new xbasic::MainLoop);
+    auto window = mainLoop->createWindow(settings);
+    mainLoop->run(window, std::make_shared<xbasic::AppBase>());
+    mainLoop->loop();
     xbasic::xlog::ShutdownLogging();
 
     return 0;
